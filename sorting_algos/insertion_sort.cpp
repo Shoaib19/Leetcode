df388e4f1c9cf7de13ba2e2040456a4e14ed8733
Shoaib sabir <86943632+Shoaib19@users.noreply.github.com>
2023-03-29 22:29:11 +0500

    added another approach

--- sorting_algos/insertion_sort.cpp
+++ sorting_algos/insertion_sort.cpp
@@ -14,3 +14,20 @@ void insertion_sort(int arr[],int size)
     }
     
 }
+
+
+// another apporach apparently easy one
+
+void insertion_sort(int arr[], int size){
+    for(int i = 1; i < size ; i++)
+    {
+       int j = i;
+        while(j > 0 && arr[j - 1] > arr[j])
+        {
+            arr[j] = arr[j] + arr[j-1];
+            arr[j-1] = arr[j] - arr[j-1];
+            arr[j] = arr[j] - arr[j-1];
+            j--;
+        }
+    }
+}
