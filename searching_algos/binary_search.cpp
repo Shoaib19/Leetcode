11ca583817dc69a2452641f17fda7490eecda575
Shoaib19 <shoaibsabir019@gmail.com>
2023-04-10 23:37:15 +0500

    incomplete binary search

--- searching_algos/binary_search.cpp
+++ searching_algos/binary_search.cpp
@@ -1,4 +1,17 @@
-void binary_search(int arr[],int size, int key)
+int binary_search(int arr[],int size, int key)
 {
-  // code here
+    int mid =  size / 2;
+    if(arr[mid] == key)
+    return mid;
+    while(mid > 0 && mid < size)
+    {
+        if(key < arr[mid])
+        {
+            mid = mid / 2;
+        }
+        else if(key > arr[mid])
+        {
+            
+        }
+    }
 }
