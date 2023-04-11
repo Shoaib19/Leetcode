8270d86b6137e959c30df159607056a1d9f5dabe
Shoaib19 <shoaibsabir019@gmail.com>
2023-04-11 22:58:55 +0500

    first try binary search

--- searching_algos/binary_search.cpp
+++ searching_algos/binary_search.cpp
@@ -3,15 +3,21 @@ int binary_search(int arr[],int size, int key)
     int mid =  size / 2;
     if(arr[mid] == key)
     return mid;
-    while(mid > 0 && mid < size)
+    int low = 0;
+    int high = size - 1;
+    
+    while(arr[mid] != key)
     {
         if(key < arr[mid])
         {
-            mid = mid / 2;
+            high = mid;
+            mid = low + high / 2;
         }
         else if(key > arr[mid])
         {
-            
+            low = mid;
+            mid = low + high / 2;
         }
     }
+    return (arr[mid] == key) ? mid : -1;
 }
